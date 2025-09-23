d68432d30ea0b6a96bbede1bc1daada7dd5e64ff
Yasinahamad <163731345+yasinahamed2005@users.noreply.github.com>
2025-09-23 21:50:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Replace '/your-app/' with the path your app is served from
 export default defineConfig({
-  base: '/', 
+  base: '/', // keep '/' if app is at root URL
   plugins: [react(), tailwindcss()],
 })
