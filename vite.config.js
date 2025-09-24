d60876d346835bae17a41e1f623447e7cff154bc
Yasinahamad <163731345+yasinahamed2005@users.noreply.github.com>
2025-09-24 07:22:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 👇 important: set this to match your Tomcat context path
-  base: '/Taskflow/', 
-
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  build: {
-    outDir: 'dist',       // default build output
-    assetsDir: 'assets',  // keep assets in /assets
-  },
-
-  server: {
-    port: 5173,           // dev server port
-    open: true,           // auto-open in browser on dev
-  },
+  plugins: [react(),
+  tailwindcss(),],
 })
